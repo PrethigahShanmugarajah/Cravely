3ad7c3fe90c2ebb60bacd82f83fcf604a0d3a6fb
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-30 21:14:25 +0530

    Add AboutPage

---
+++ Client/src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,11 @@
+// Craverly / Client / src / pages / AboutPage / AboutPage.jsx
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1 className="text-teal-500 text-2xl bg-[#0F172A]">AboutPage</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
