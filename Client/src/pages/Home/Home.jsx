b4dda09895c051f99834042332052cbda8b78b49
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-30 21:15:09 +0530

    Add Home page

---
+++ Client/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+// Craverly / Client / src / pages / Home / Home.jsx
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-teal-500 text-2xl bg-[#0F172A]">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
