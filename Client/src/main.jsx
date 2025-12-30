8074244d99a367e9bc1f466adaaca06278de79c1
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-30 20:58:49 +0530

    Set up CartProvider in main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,11 +1,14 @@
-// Client / src / main.jsx
+// Cravely / Client / src / main.jsx
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { CartProvider } from "./CartContext/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <CartProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </CartProvider>
 );
