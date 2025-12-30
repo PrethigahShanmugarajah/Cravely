c59614263146cefcba4b3e0fc01b28657a485982
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-30 21:15:14 +0530

    Add Menu page

---
+++ Client/src/pages/Menu/Menu.jsx
@@ -0,0 +1,11 @@
+// Craverly / Client / src / pages / Menu / Menu.jsx
+
+const Menu = () => {
+  return (
+    <div>
+      <h1 className="text-teal-500 text-2xl bg-[#0F172A]">Menu</h1>
+    </div>
+  );
+};
+
+export default Menu;
