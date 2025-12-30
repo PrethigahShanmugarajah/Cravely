df0466aa87d493acc1138586fb072055d7762edf
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-30 21:14:40 +0530

    Add Cart page

---
+++ Client/src/pages/Cart/Cart.jsx
@@ -0,0 +1,11 @@
+// Craverly / Client / src / pages / Cart / Cart.jsx
+
+const Cart = () => {
+  return (
+    <div>
+      <h1 className="text-teal-500 text-2xl bg-[#0F172A]">Cart</h1>
+    </div>
+  );
+};
+
+export default Cart;
