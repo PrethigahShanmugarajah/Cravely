efebe405e1db3659d4e71fb7e278f997c2975beb
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-31 00:45:18 +0530

    Add SignUp component

---
+++ Client/src/components/SignUp/SignUp.jsx
@@ -0,0 +1,11 @@
+// Craverly / Client / src / components / SignUp / SignUp.jsx
+
+const SignUp = () => {
+  return (
+    <div>
+      <h1 className="text-teal-500 text-2xl bg-[#0F172A]">SignUp</h1>
+    </div>
+  );
+};
+
+export default SignUp;
