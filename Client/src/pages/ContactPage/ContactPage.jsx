767cc537060470e5bfad245b60e1fb4001e215f4
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-30 21:14:58 +0530

    Add ContactPage

---
+++ Client/src/pages/ContactPage/ContactPage.jsx
@@ -0,0 +1,11 @@
+// Craverly / Client / src / pages / ContactPage / ContactPage.jsx
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1 className="text-teal-500 text-2xl bg-[#0F172A]">ContactPage</h1>
+    </div>
+  );
+};
+
+export default ContactPage;
