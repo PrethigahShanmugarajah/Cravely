20b0dd6c2ad821a77317d279357ea6bcf1b196af
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-31 14:45:03 +0530

    Add SpecialOffer component to Home page below Banner

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 // Craverly / Client / src / pages / Home / Home.jsx
 import Banner from "../../components/Banner/Banner";
+import SpecialOffer from "../../components/SpecialOffer/SpecialOffer";
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <SpecialOffer />
     </>
   );
 };
