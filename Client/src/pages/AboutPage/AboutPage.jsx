80321b269b289e91b928cf9fae7b72a2d5386c08
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-01 01:12:14 +0530

    Add AboutPage wrapper component for About section

--- Client/src/pages/AboutPage/AboutPage.jsx
+++ Client/src/pages/AboutPage/AboutPage.jsx
@@ -1,10 +1,11 @@
 // Craverly / Client / src / pages / AboutPage / AboutPage.jsx
+import About from "../../components/About/About";
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1 className="text-teal-500 text-2xl bg-[#0F172A]">AboutPage</h1>
-    </div>
+    <>
+      <About />
+    </>
   );
 };
 
