ff79b213a7da083085927f43492026cedeb03f6f
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-01 17:27:31 +0530

    Protect Cart route using PrivateRoute component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -8,6 +8,7 @@ import Menu from "./pages/Menu/Menu";
 import Cart from "./pages/Cart/Cart";
 import SignUp from "./components/SignUp/SignUp";
 import Footer from "./components/Footer/Footer";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 
 const App = () => {
   return (
@@ -18,9 +19,16 @@ const App = () => {
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/menu" element={<Menu />} />
-        <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/cart"
+          element={
+            <PrivateRoute>
+              <Cart />
+            </PrivateRoute>
+          }
+        />
       </Routes>
       <Footer />
     </>
