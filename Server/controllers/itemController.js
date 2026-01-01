4e9ec99f377ab82f4f92eeddfd518832c54d6371
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-01 21:43:27 +0530

    Create function for Create Item in itemController

---
+++ Server/controllers/itemController.js
@@ -0,0 +1,41 @@
+// Cravely / Server / controllers / itemController.js
+import itemModel from "../models/itemModel.js";
+
+/* -------- Create Item -------- */
+export const createItem = async (req, res) => {
+  try {
+    const { name, description, category, price, rating, hearts } = req.body;
+    const imageUrl = req.file ? `/uploads/${req.file.filename}` : "";
+
+    const total = Number(price) * 1;
+
+    const newItem = new itemModel({
+      name,
+      description,
+      category,
+      price,
+      rating,
+      hearts,
+      imageUrl,
+      total,
+    });
+
+    const item = await newItem.save();
+
+    return res
+      .status(201)
+      .json({ success: true, message: "Item created successfully!", item });
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(409).json({ message: "Item name already exists" });
+    }
+
+    console.error("Create Item Error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: "Failed to Create Item",
+      error: `Create Item Error: ${error.message}`,
+    });
+  }
+};
