e52da0c7f3be9fb360859469ec838372e8c79d1a
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-01 23:47:32 +0530

    Add deleteCartItem route in cartRoutes

--- Server/routes/cartRoutes.js
+++ Server/routes/cartRoutes.js
@@ -2,6 +2,7 @@
 import express from "express";
 import {
   addToCart,
+  deleteCartItem,
   getCart,
   updateCartItem,
 } from "../controllers/cartController.js";
@@ -12,5 +13,6 @@ const cartRouter = express.Router();
 cartRouter.get("/get", authMiddleware, getCart);
 cartRouter.post("/add", authMiddleware, addToCart);
 cartRouter.put("/update/:id", authMiddleware, updateCartItem);
+cartRouter.delete("/delete/:id", authMiddleware, deleteCartItem);
 
 export default cartRouter;
