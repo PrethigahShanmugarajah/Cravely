78b1aab40ddce0ba0dac306fbe3bb7243b0ba41c
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-01 21:43:35 +0530

    Add createItem route in itemRoutes

---
+++ Server/routes/itemRoutes.js
@@ -0,0 +1,17 @@
+// Cravely / Server / routes / itemRoutes.js
+import express from "express";
+import multer from "multer";
+import { createItem } from "../controllers/itemController.js";
+
+const itemRouter = express.Router();
+
+const storage = multer.diskStorage({
+  destination: (_req, _file, cb) => cb(null, "uploads/"),
+  filename: (_req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
+});
+
+const upload = multer({ storage });
+
+itemRouter.post("/create", upload.single("image"), createItem);
+
+export default itemRouter;
