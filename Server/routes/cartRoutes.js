fcbe6f81b335a93623ebcf52b16e5fb4fcf1ed9f
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-01 23:40:34 +0530

    Add updateCartItem route in cartRoutes

--- Server/routes/cartRoutes.js
+++ Server/routes/cartRoutes.js
@@ -1,11 +1,16 @@
 // Cravely / Server / routes / cartRoutes.js
 import express from "express";
-import { addToCart, getCart } from "../controllers/cartController.js";
+import {
+  addToCart,
+  getCart,
+  updateCartItem,
+} from "../controllers/cartController.js";
 import authMiddleware from "../middlewares/auth.js";
 
 const cartRouter = express.Router();
 
 cartRouter.get("/get", authMiddleware, getCart);
 cartRouter.post("/add", authMiddleware, addToCart);
+cartRouter.put("/update/:id", authMiddleware, updateCartItem);
 
 export default cartRouter;
