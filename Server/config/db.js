ac48c675a23883cbf0145c4f263e4716ecb44a7d
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-01 18:14:49 +0530

    Setup MongoDB connection using mongoose

---
+++ Server/config/db.js
@@ -0,0 +1,15 @@
+// Cravely / Server / config / db.js
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database Connected!")
+    );
+    await mongoose.connect(`${process.env.MONGODB_URI}/Cravely`);
+  } catch (error) {
+    console.log("Database Connection Error:", error.message);
+  }
+};
+
+export default connectDB;
