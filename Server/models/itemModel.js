d9b489607ce635c431617d6f1d8bb97981ced67c
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-01 20:14:01 +0530

    Create Item model

---
+++ Server/models/itemModel.js
@@ -0,0 +1,20 @@
+// Cravely / Server / models / itemModel.js
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    category: { type: String, required: true },
+    price: { type: String, required: true },
+    rating: { type: String, default: 0 },
+    hearts: { type: String, default: 0 },
+    total: { type: String, default: 0 },
+    imageUrl: { type: String },
+  },
+  { timestamps: true }
+);
+
+const itemModel = mongoose.models.item || mongoose.model("item", itemSchema);
+
+export default itemModel;
