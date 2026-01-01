82d017571045ca0f8d15816d2df3bf51906d5e7f
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-01 23:03:44 +0530

    Add addToCart route in cartRoutes

--- Server/routes/cartRoutes.js
+++ Server/routes/cartRoutes.js
@@ -1,10 +1,11 @@
 // Cravely / Server / routes / cartRoutes.js
 import express from "express";
-import { getCart } from "../controllers/cartController.js";
+import { addToCart, getCart } from "../controllers/cartController.js";
 import authMiddleware from "../middlewares/auth.js";
 
 const cartRouter = express.Router();
 
 cartRouter.get("/get", authMiddleware, getCart);
+cartRouter.post("/add", authMiddleware, addToCart);
 
 export default cartRouter;
