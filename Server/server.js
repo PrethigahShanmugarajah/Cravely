9314c9e552a199b0f9c68d05657eee13baea702b
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-01 19:17:46 +0530

    Add user routes and configure CORS with allowed frontend URLs

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 /* -------- INITIALIZE EXPRESS -------- */
 const app = express();
@@ -13,10 +14,26 @@ connectDB();
 /* -------- MIDDLEWARE CONFIGURATION -------- */
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extend: true }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigin = process.env.FRONTEND_URLS.split(",");
+
+      if (!origin || allowedOrigin.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 /* -------- ROUTES -------- */
 app.get("/", (req, res) => res.send("API is Working!"));
+app.use("/api/user", userRouter);
 
 /* -------- PORT -------- */
 const port = process.env.PORT || 4000;
