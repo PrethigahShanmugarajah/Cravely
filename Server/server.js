1153ce66a3058e06e6431d2ae7ccf3ddcd4ab64c
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-01 18:16:45 +0530

    Setup MongoDB connection in server.js

--- Server/server.js
+++ Server/server.js
@@ -2,11 +2,13 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 /* -------- INITIALIZE EXPRESS -------- */
 const app = express();
 
 /* -------- CONNECT TO DATABASE -------- */
+connectDB();
 
 /* -------- MIDDLEWARE CONFIGURATION -------- */
 app.use(express.json());
