23daa24638dfbead7f6b66a67bca9bc821a2b654
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-23 22:45:28 +0530

    Set up routing for AddItems, List, and Order components in App

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,10 +1,19 @@
 // Craverly /  Admin / src / App.jsx
+import AddItems from "./components/AddItems/AddItems";
+import List from "./components/List/List";
 import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Order from "./components/Order/Order";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<AddItems />} />
+        <Route path="/list" element={<List />} />
+        <Route path="/orders" element={<Order />} />
+      </Routes>
     </div>
   );
 };
