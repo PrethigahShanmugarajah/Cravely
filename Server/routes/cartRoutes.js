5190986297dfa6b530b70b26b147be4d0d6c25f3
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-23 20:13:02 +0530

    Update cart routes to match renamed cart controller functions

--- Server/routes/cartRoutes.js
+++ Server/routes/cartRoutes.js
@@ -1,20 +1,20 @@
 // Cravely / Server / routes / cartRoutes.js
 import express from "express";
 import {
-  addToCart,
-  clearCart,
-  deleteCartItem,
-  getCart,
-  updateCartItem,
+  cartAdd,
+  cartClear,
+  cartDeleteItem,
+  cartGet,
+  cartUpdateItem,
 } from "../controllers/cartController.js";
 import authMiddleware from "../middlewares/auth.js";
 
 const cartRouter = express.Router();
 
-cartRouter.get("/get", authMiddleware, getCart);
-cartRouter.post("/add", authMiddleware, addToCart);
-cartRouter.put("/update/:id", authMiddleware, updateCartItem);
-cartRouter.delete("/delete/:id", authMiddleware, deleteCartItem);
-cartRouter.delete("/clear", authMiddleware, clearCart);
+cartRouter.get("/cart-get", authMiddleware, cartGet);
+cartRouter.post("/cart-add", authMiddleware, cartAdd);
+cartRouter.put("/cart-update-item/:id", authMiddleware, cartUpdateItem);
+cartRouter.delete("/cart-delete-item/:id", authMiddleware, cartDeleteItem);
+cartRouter.delete("/cart-clear", authMiddleware, cartClear);
 
 export default cartRouter;
