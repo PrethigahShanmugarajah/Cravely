d6400b2822b857076b8e01e8adf05c7242cda05d
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-24 03:12:36 +0530

    Fix item create API route path

--- Admin/src/api/api_route.js
+++ Admin/src/api/api_route.js
@@ -3,7 +3,7 @@ const BASE_URL = import.meta.env.VITE_BASEURL;
 
 const API_ROUTES = {
   ITEM: {
-    ITEM_CREATE: `${BASE_URL}/api/item/item-create`,
+    ITEM_CREATE: `${BASE_URL}/api/items/item-create`,
   },
 };
 export default API_ROUTES;
