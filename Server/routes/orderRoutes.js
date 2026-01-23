1920bd949307fa3c56901484b6bab0c889b9031f
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-23 19:02:02 +0530

    Add ordersGet route in orderRoutes

--- Server/routes/orderRoutes.js
+++ Server/routes/orderRoutes.js
@@ -4,11 +4,13 @@ import authMiddleware from "../middlewares/auth.js";
 import {
   orderConfirmPayment,
   orderCreate,
+  ordersGet,
 } from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/order-create", authMiddleware, orderCreate);
 orderRouter.get("/order-confirm-payment", authMiddleware, orderConfirmPayment);
+orderRouter.get("/orders-get", authMiddleware, ordersGet);
 
 export default orderRouter;
