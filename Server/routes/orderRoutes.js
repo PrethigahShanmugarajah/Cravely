a081f2c723dcff7083be93537ad94285c002cf50
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-23 18:50:34 +0530

    Add orderConfirmPayment route in orderRoutes

--- Server/routes/orderRoutes.js
+++ Server/routes/orderRoutes.js
@@ -1,10 +1,14 @@
 // Cravely / Server / routes / orderRoutes.js
 import express from "express";
 import authMiddleware from "../middlewares/auth.js";
-import { orderCreate } from "../controllers/orderController.js";
+import {
+  orderConfirmPayment,
+  orderCreate,
+} from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/order-create", authMiddleware, orderCreate);
+orderRouter.get("/order-confirm-payment", authMiddleware, orderConfirmPayment);
 
 export default orderRouter;
