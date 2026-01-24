27ad737be25c73d7daffcee502f8872720eb2cf3
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-24 16:48:00 +0530

    Add ITEM_GET_ALL API route

--- Admin/src/api/api_route.js
+++ Admin/src/api/api_route.js
@@ -4,6 +4,7 @@ const BASE_URL = import.meta.env.VITE_BASEURL;
 const API_ROUTES = {
   ITEM: {
     ITEM_CREATE: `${BASE_URL}/api/items/item-create`,
+    ITEM_GET_ALL: `${BASE_URL}/api/items/item-get-all`,
   },
 };
 export default API_ROUTES;
