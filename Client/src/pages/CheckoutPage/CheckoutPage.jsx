ec0a1f14ffe989153cdfa27917de46f686dd7eec
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-25 19:39:22 +0530

    Add basic CheckoutPage component

---
+++ Client/src/pages/CheckoutPage/CheckoutPage.jsx
@@ -0,0 +1,11 @@
+// Cravely / Client / src / pages / CheckoutPage / CheckoutPage.jsx
+
+const CheckoutPage = () => {
+  return (
+    <div>
+      <h1>CheckoutPage</h1>
+    </div>
+  );
+};
+
+export default CheckoutPage;
