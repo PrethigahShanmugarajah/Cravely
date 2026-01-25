a77ce9a423672c901b1c90143115253224ac071f
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-25 19:40:36 +0530

    Add basic VerifyPaymentPage component

---
+++ Client/src/pages/VerifyPaymentPage/VerifyPaymentPage.jsx
@@ -0,0 +1,11 @@
+// Cravely / Client / src / pages / VerifyPaymentPage / VerifyPaymentPage.jsx
+
+const VerifyPaymentPage = () => {
+  return (
+    <div>
+      <h1>VerifyPaymentPage</h1>
+    </div>
+  );
+};
+
+export default VerifyPaymentPage;
