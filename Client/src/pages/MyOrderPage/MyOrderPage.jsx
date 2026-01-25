141fb8e183ec244302c499c5d28a035d174ca21e
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-25 19:38:31 +0530

    Add basic MyOrderPage component

---
+++ Client/src/pages/MyOrderPage/MyOrderPage.jsx
@@ -0,0 +1,11 @@
+// Cravely / Client / src / pages / MyOrderPage / MyOrderPage.jsx
+
+const MyOrderPage = () => {
+  return (
+    <div>
+      <h1>MyOrderPage</h1>
+    </div>
+  );
+};
+
+export default MyOrderPage;
