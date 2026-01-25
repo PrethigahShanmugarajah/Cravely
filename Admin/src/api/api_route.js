197068c4c0949bec4db49e11d90224c537c42b28
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-25 19:18:07 +0530

    Add ORDER_ADMIN_GET_ALL and ORDER_UPDATE_ADMIN_GET_ALL API routes

--- Admin/src/api/api_route.js
+++ Admin/src/api/api_route.js
@@ -7,5 +7,11 @@ const API_ROUTES = {
     ITEM_GET_ALL: `${BASE_URL}/api/items/item-get-all`,
     ITEM_DELETE: (itemId) => `${BASE_URL}/api/items/item-delete/${itemId}`,
   },
+
+  ORDER: {
+    ORDER_ADMIN_GET_ALL: `${BASE_URL}/api/order/order-admin-get-all`,
+    ORDER_UPDATE_ADMIN_GET_ALL: (orderId) =>
+      `${BASE_URL}/api/order/order-update-any-by-admin/${orderId}`,
+  },
 };
 export default API_ROUTES;
