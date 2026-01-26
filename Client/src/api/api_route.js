f4cf62c7554f0f6b0dc4cdb7ec4f4a5cbce8eb85
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-26 23:27:48 +0530

    Add ORDER_CONFIRM_PAYMENT and ORDER_CREATE API routes

--- Client/src/api/api_route.js
+++ Client/src/api/api_route.js
@@ -9,12 +9,19 @@ const API_ROUTES = {
     CART_UPDATE_ITEM: (id) => `${BASE_URL}/api/cart/cart-update-item/${id}`,
     CART_CLEAR: `${BASE_URL}/api/cart/cart-clear`,
   },
+
   USER: {
     USER_REGISTER: `${BASE_URL}/api/user/user-register`,
     USER_LOGIN: `${BASE_URL}/api/user/user-login`,
   },
+
   ITEM: {
     ITEM_GET_ALL: `${BASE_URL}/api/items/item-get-all`,
+  },
+
+  ORDER: {
+    ORDER_CONFIRM_PAYMENT: `${BASE_URL}/api/order/order-confirm-payment`,
+    ORDER_CREATE: `${BASE_URL}/api/order/order-create`,
   },
 };
 export default API_ROUTES;
